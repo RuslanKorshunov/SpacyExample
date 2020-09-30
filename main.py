7ccd0420bbf540a7dbe5cd0b4f2c4989e25985ad
RuslanKorshunov <ruslan.korsh@yandex.ru>
2020-09-30 11:40:16 +0300

    fix/rewrite text input

--- main.py
+++ main.py
@@ -4,22 +4,16 @@ nlp = spacy.load("en_core_web_sm")
 
 
 def tokenize(text):
-    doc1 = nlp(text.decode('utf8'))
+    doc = nlp(text)
     print("Result")
-    for token in doc1:
+    for token in doc:
         print("Text: " + token.text + ", lemma: " + token.lemma_ + ", part of speech: " + token.pos_)
 
 
 def main():
-    text1 = "Apple is looking at buying U.K. startup for $25 billion dollars."
-    print("Text1\n" + text1)
-    tokenize(text1)
-
-    text2 = "The drawbacks of classical mathematical logic and automated reasoning are discussed. " \
-            "The difference between classical logic and human cognition is shown on simple examples. " \
-            "Information granulation based on pragmatics is viewed as a principal capacity of cognitive agent."
-    print("Text2\n" + text2)
-    tokenize(text2)
+    text = input("Enter text:")
+    print("\nText\n" + text)
+    tokenize(text)
 
 
 if __name__ == "__main__":
